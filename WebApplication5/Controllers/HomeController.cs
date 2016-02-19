25548e8e044c7e041d54cdb5135fecb3a720a6b4
James Sturtevant <jsturtevant@gmail.com>
2016-02-19 15:23:34 -0500

    initial commit with profile image

---
+++ WebApplication5/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication5.Models;
+using Microsoft.AspNet.Identity.Owin;
+
+namespace WebApplication5.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+      
+    }
+}
